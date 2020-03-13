8af98919368c8c882efc2b4a20e2357e050e1519
Manik Magar <manik.magar@gmail.com>
2020-03-13 00:35:18 -0400

    Add tests

--- src/test/java/com/github/manikmagar/mule/flowdiagrams/drawings/GraphDiagramTest.java
+++ src/test/java/com/github/manikmagar/mule/flowdiagrams/drawings/GraphDiagramTest.java
@@ -2,8 +2,11 @@ package com.github.manikmagar.mule.flowdiagrams.drawings;
 
 import com.github.manikmagar.mule.flowdiagrams.model.FlowContainer;
 import com.github.manikmagar.mule.flowdiagrams.model.MuleComponent;
+import io.github.netmikey.logunit.api.LogCapturer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.event.Level;
 
 import java.io.File;
 import java.util.Arrays;
@@ -16,6 +19,10 @@ class GraphDiagramTest {
   @TempDir
   File tempDir;
 
+  @RegisterExtension
+  LogCapturer logs = LogCapturer.create().captureForType(GraphDiagram.class, Level.DEBUG);
+
+
   @Test
   void draw() {
     File output = new File(tempDir, "output.png");
@@ -25,13 +32,18 @@ class GraphDiagramTest {
     FlowContainer flowContainer = new FlowContainer("flow", "test-flow");
     flowContainer.addComponent(new MuleComponent("flow-ref", "test-sub-flow"));
     FlowContainer subflow = new FlowContainer("sub-flow", "test-sub-flow");
+
+    // Add reference to same sub-flow, resulting loop
+    subflow.addComponent(new MuleComponent("flow-ref", "test-sub-flow"));
+
     context.setComponents(Arrays.asList(flowContainer, subflow));
     context.setKnownComponents(Collections.emptyMap());
 
     GraphDiagram graphDiagram = new GraphDiagram();
     graphDiagram.draw(context);
-    System.out.println(output);
+
     assertThat(output).exists();
+    logs.assertContains("Detected a possible self loop in sub-flow test-sub-flow. Skipping flow-ref processing.");
   }
 
   @Test
